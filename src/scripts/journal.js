b337da3facaff79985d8391c2b6d2efac7c972d7
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-16 11:03:36 -0500

    added an array variable and several days of notes

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,6 +1,23 @@
-const journalEntry = {
-    dateJournalEntry:
-    conceptsJournalEntry:
-    contentsJournalEntry:
-    moodJournalEntry:
+const journalEntry0 = {
+    dateJournalEntry: 07/08/2019,
+    conceptsJournalEntry: "Semantic Tags",
+    contentsJournalEntry: "We discussed semantic tags and their purpose",
+    moodJournalEntry: "happy",
 }
+const journalEntry1 = {
+    dateJournalEntry: 07/09/2019,
+    conceptsJournalEntry: "Flex Box",
+    contentsJournalEntry: "We learned more about Flexbox",
+    moodJournalEntry: "happy",
+}
+const journalEntry2 = {
+    dateJournalEntry: 07/10/2019,
+    conceptsJournalEntry: "Group Projects",
+    contentsJournalEntry: "We learned about group projects",
+    moodJournalEntry: "happy",
+}
+
+let carlsJournal = []
+
+carlsJournal.push(journalEntry0, journalEntry1, journalEntry2)
+console.log(carlsJournal)
