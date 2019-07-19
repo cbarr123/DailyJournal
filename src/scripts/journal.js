a035d474de87f2fb14b69c8d47714567625aa679
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-19 14:54:17 -0500

    fixed the logic, with the forEach loops

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,23 +1,21 @@
-
-
 let journalEntries = [
     {
-        dateJournalEntry: 07/08/2019,
-        conceptsJournalEntry: "Semantic Tags",
-        contentsJournalEntry: "We discussed semantic tags and their purpose",
-        moodJournalEntry: "happy",
+        date: "07/08/2019",
+        concept: "Semantic Tags",
+        contents: "We discussed semantic tags and their purpose",
+        mood: "happy"
     },
     {
-        dateJournalEntry: 07/09/2019,
-        conceptsJournalEntry: "Flex Box",
-        contentsJournalEntry: "We learned more about Flexbox",
-        moodJournalEntry: "happy",
+        date: "07/09/2019",
+        concept: "Flex Box",
+        contents: "We learned more about Flexbox",
+        mood: "happy"
     },
     {
-        dateJournalEntry: 07/10/2019,
-        conceptsJournalEntry: "Group Projects",
-        contentsJournalEntry: "We learned about group projects",
-        moodJournalEntry: "happy",
+        date: "07/10/2019",
+        concept: "Group Projects",
+        contents: "We learned about group projects",
+        mood: "happy"
     },
 ]
 
@@ -25,18 +23,18 @@ let journalEntries = [
 const whereToDisplayTheJournalEntriesInTheDOM = document.querySelector("#journalContainer");
 
 //* create a representation of the data
-const createJournalEntryComponent = (journalEntries) => {
-    // return 
-        `<div class="journalEntry">
-            <h2>${journalEntries.dateJournalEntry}</h2>
-            <section>${journalEntries.conceptsJournalEntry}</section>
-            <section>${journalEntries.contentsJournalEntry}</section>
-            <section>${journalEntries.moodJournalEntry}</section>
+const createJournalEntryComponent = (date, concept, contents, mood) => {
+        return `<div class="journalEntry">
+            <h2>${date}</h2>
+            <section>Concepts Covered: ${concept}</section>
+            <section>${contents}</section>
+            <section>${mood}</section>
         </div>`
 }
 //* invoke the function
 //* inject the HTML into the DOM
-journalEntries.forEach(entry => {
-   const htmlRepresentation = createJournalEntryComponent(journalEntries);
+
+journalEntries.forEach(journalEntry => {
+   const htmlRepresentation = createJournalEntryComponent(journalEntry.date, journalEntry.concept, journalEntry.contents, journalEntry.mood);
    whereToDisplayTheJournalEntriesInTheDOM.innerHTML += htmlRepresentation;
 })
