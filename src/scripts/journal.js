e10e1996a4ee9b8cc018b42a32d3409e8edf32db
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-19 11:38:22 -0500

    added iteration through array

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,23 +1,42 @@
-const journalEntry0 = {
-    dateJournalEntry: 07/08/2019,
-    conceptsJournalEntry: "Semantic Tags",
-    contentsJournalEntry: "We discussed semantic tags and their purpose",
-    moodJournalEntry: "happy",
-}
-const journalEntry1 = {
-    dateJournalEntry: 07/09/2019,
-    conceptsJournalEntry: "Flex Box",
-    contentsJournalEntry: "We learned more about Flexbox",
-    moodJournalEntry: "happy",
-}
-const journalEntry2 = {
-    dateJournalEntry: 07/10/2019,
-    conceptsJournalEntry: "Group Projects",
-    contentsJournalEntry: "We learned about group projects",
-    moodJournalEntry: "happy",
-}
 
-let carlsJournal = []
 
-carlsJournal.push(journalEntry0, journalEntry1, journalEntry2)
-console.log(carlsJournal)
+let journalEntries = [
+    {
+        dateJournalEntry: 07/08/2019,
+        conceptsJournalEntry: "Semantic Tags",
+        contentsJournalEntry: "We discussed semantic tags and their purpose",
+        moodJournalEntry: "happy",
+    },
+    {
+        dateJournalEntry: 07/09/2019,
+        conceptsJournalEntry: "Flex Box",
+        contentsJournalEntry: "We learned more about Flexbox",
+        moodJournalEntry: "happy",
+    },
+    {
+        dateJournalEntry: 07/10/2019,
+        conceptsJournalEntry: "Group Projects",
+        contentsJournalEntry: "We learned about group projects",
+        moodJournalEntry: "happy",
+    },
+]
+
+//* create a reference of where to put the data
+const whereToDisplayTheJournalEntriesInTheDOM = document.querySelector("#journalContainer");
+
+//* create a representation of the data
+const createJournalEntryComponent = (journalEntries) => {
+    // return 
+        `<div class="journalEntry">
+            <h2>${journalEntries.dateJournalEntry}</h2>
+            <section>${journalEntries.conceptsJournalEntry}</section>
+            <section>${journalEntries.contentsJournalEntry}</section>
+            <section>${journalEntries.moodJournalEntry}</section>
+        </div>`
+}
+//* invoke the function
+//* inject the HTML into the DOM
+journalEntries.forEach(entry => {
+   const htmlRepresentation = createJournalEntryComponent(journalEntries);
+   whereToDisplayTheJournalEntriesInTheDOM.innerHTML += htmlRepresentation;
+})
