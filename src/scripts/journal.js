87ef34c168c5b35af4038135d854bd2d95e88780
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-08-02 08:42:50 -0500

    completed chapter 4, json server is working and posting

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,22 +1,33 @@
 //* fetch the data from the json object
-
+fetch("http://localhost:3000/entries")
+  .then(entries => entries.json())
+  .then(journalEntries => {
+    console.table(journalEntries);
+    journalEntries.forEach(journalEntry => {
+        const htmlRepresentation = createJournalEntryComponent(
+          journalEntry.date,
+          journalEntry.concept,
+          journalEntry.contents,
+          journalEntry.mood
+        );
+        whereToDisplayTheJournalEntriesInTheDOM.innerHTML += htmlRepresentation;
+      });
+  });
 
 //* create a reference of where to put the data
-const whereToDisplayTheJournalEntriesInTheDOM = document.querySelector("#journalContainer");
+const whereToDisplayTheJournalEntriesInTheDOM = document.querySelector(
+  "#journalContainer"
+);
 
 //* create a representation of the data
 const createJournalEntryComponent = (date, concept, contents, mood) => {
-        return `<div class="journalEntry">
+  return `<div class="journalEntry">
             <h2>${date}</h2>
             <section>Concepts Covered: ${concept}</section>
             <section>${contents}</section>
             <section>${mood}</section>
-        </div>`
-}
+        </div>`;
+};
 //* invoke the function
 //* inject the HTML into the DOM
 
-journalEntries.forEach(journalEntry => {
-   const htmlRepresentation = createJournalEntryComponent(journalEntry.date, journalEntry.concept, journalEntry.contents, journalEntry.mood);
-   whereToDisplayTheJournalEntriesInTheDOM.innerHTML += htmlRepresentation;
-})
